e7391c5ad8c2420464dc9efc759c3f953c5fd65d
ceddybi <ceddymuhoza@gmail.com>
2024-01-27 10:52:01 -0500

    nav

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,11 @@
 import { ClientProcess } from "./appx";
+import Navbar from "./navbar";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
 root.render(
-  <div className="p-5">
-    <h1 className="bg-gray-500 text-center text-white">
-      Hi Tailwind has been integrated.
-    </h1>
-    <h2>Hello from React!</h2>
+  <div className="h-full bg-gray-50">
+    <Navbar user={null} />
     <ClientProcess />
   </div>
 );
